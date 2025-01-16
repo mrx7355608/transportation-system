0f7c8db75f4607456ccb0aeecc2eab3ba6e21f03
mrx7355608 <mrx7355608@gmail.com>
2025-01-16 16:35:27 +0500

    registered models in main application

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const { GraphQLApp } = require("@keystonejs/app-graphql");
 const { AdminUIApp } = require("@keystonejs/app-admin-ui");
 const { keystone } = require("./keystone");
 const { PasswordAuthStrategy } = require("@keystonejs/auth-password");
-const { userModel } = require("./models/user");
 const { config } = require("./utils/config");
+const { userModel } = require("./models/user");
+const { studentModel } = require("./models/student");
+const { driverModel } = require("./models/driver");
+const { feeModel } = require("./models/fee");
+const { routeModel } = require("./models/route");
+const { vehicleModel } = require("./models/vehicle");
 
 // Create keystone models
 keystone.createList("User", userModel);
+keystone.createList("Driver", driverModel);
+keystone.createList("Student", studentModel);
+keystone.createList("Fee", feeModel);
+keystone.createList("Route", routeModel);
+keystone.createList("Vehicle", vehicleModel);
 
 const PROJECT_NAME = "Transportation System";
 
